2d656de85d9a82e54f826d3ecea97fc7646c2e13
Vikrant Zine <vikrantzine@gmail.com>
2024-10-03 17:24:36 +0530

    commit

--- Apex Programs/Apex Manifest/force-app/main/default/lwc/task/task.js
+++ Apex Programs/Apex Manifest/force-app/main/default/lwc/task/task.js
@@ -1,11 +1,3 @@
 import { LightningElement } from 'lwc';
 
-export default class FinancialDashboard extends LightningElement {
-    // Properties to bind to the HTML template
-    ratingCode = '5AA4';
-    riskClass = 'high-risk'; // You can change this based on conditions
-    riskLevel = 'High Risk';
-    financialEmbarrassment = 'A Financial Embarrassment Present';
-    failureScore = 78;
-    paymentDays = 35;
-}
+export default class CreditRiskAssessment extends LightningElement {}
